8ee9e6784244023dd8f23008b9bb8523de8af5ab
Lora <cruzeiro@abv.bg>
2021-02-07 16:49:43 +0200

    Initial Commit

---
+++ src/InterfacesAndAbstraction_Exercises/DefineAnInterfacePerson/Main.java
@@ -0,0 +1,7 @@
+package InterfacesAndAbstraction_Exercises.DefineAnInterfacePerson;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/InterfacesAndAbstraction_Exercises/DefineAnInterfacePerson/Person.java
@@ -0,0 +1,6 @@
+package InterfacesAndAbstraction_Exercises.DefineAnInterfacePerson;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
